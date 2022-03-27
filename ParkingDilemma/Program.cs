b76b6b2c9b09972d41a88a2b8cedfaa450d62ea8
Rovshan_C# <gamerazez97@gmail.com>
2022-03-27 17:35:36 +0400

    Csharp Parking Dilemma

---
+++ ParkingDilemma/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ParkingDilemma;
+public static class Solution {
+    private static int CarParkingRoof(int[] arr, int k) {
+        Array.Sort(arr);
+        var start = 0;
+        var carParkingRoof = int.MaxValue;
+        
+        for (int end = 0; end < arr.Length; end++) {
+            if (end < k - 1) {
+                continue;
+            }
+            var currentRoofLength = arr[end] - arr[start++] + 1;
+            carParkingRoof = Math.Min(carParkingRoof, currentRoofLength);
+        }
+        return carParkingRoof;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(
+        CarParkingRoof(new []{6,2,12,7}, 2)); 
+    }
+}
